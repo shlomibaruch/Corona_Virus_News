74beb3d9cf291399fa9ebb6d70e0d99b65b17268
Shlomi Baruch <shlomibaruch5@gmail.com>
2020-03-19 20:04:20 +0200

    clear

--- coronavirus/src/App.js
+++ coronavirus/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
+class App extends Component {
+
+  render(){
+      return (
+        <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Hello App
+      </h1>
     </div>
+    </BrowserRouter>
   );
+  }
+
 }
 
 export default App;
