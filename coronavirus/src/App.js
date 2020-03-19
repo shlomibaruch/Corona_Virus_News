d363f0af2d86d1e2cf636539564901b6a65043cb
Shlomi Baruch <shlomibaruch5@gmail.com>
2020-03-19 20:50:25 +0200

    shlomi2

--- coronavirus/src/App.js
+++ coronavirus/src/App.js
@@ -1,20 +1,24 @@
 import React,{Component} from 'react';
 import './App.css';
+// import Articel from './components/articel/Articel'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
 
   render(){
-      return (
-        <BrowserRouter>
+    return (
+    <BrowserRouter>
     <div className="App">
       <h1>
         Hello App
       </h1>
-      <h2>coronavirus</h2>
-      <h1>coronavirus 2</h1>
+
     </div>
+    {/* < Switch>
+        <Route exact path='/articel' render={()=><Articel>} />
+      </Switch> */}
     </BrowserRouter>
+
   );
   }
 
